10121edf3d3c1f02f9981a201343d09e270badd1
Yan Sun <tsun812@gmail.com>
2022-01-18 18:19:58 -0800

    set up daylist item with storybook

---
+++ src/components/DayListItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classNames from "classnames";
+export default function DayListItem(props) {
+  return (
+    <li onClick={() => {props.setDay(props.name);}}>
+      <h2 className="text--regular">Day {props.name}.</h2> 
+      <h3 className="text--light">{props.spots} spots remaining.</h3>
+    </li>
+  );
+}
