fc3324575fe6f21e9e8a000cbe4ace0ad2c54ec0
Yan Sun <tsun812@gmail.com>
2022-01-18 19:25:37 -0800

    create unit test for daylist item, refactor daylist item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,25 @@
 import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss"
+function formatSpots(props) {
+  console.log(props.spots)
+  if (props.spots === 0){
+    return "no spots remaining"
+  }
+  else if (props.spots === 1){
+    return "1 spot remaining"
+  }
+  else if (props.spots > 1){
+    return `${props.spots} spots remaining`
+  }
+}
 export default function DayListItem(props) {
   const addClass = classNames("day-list__item", {"day-list__item--selected":props.selected, "day-list__item--full":props.spots===0})
+  const formatS = formatSpots(props);
   return (
     <li onClick={() => {props.setDay(props.name);}}>
       <h2 className={addClass}>Day {props.name}.</h2> 
-      <h3 className={addClass}>{props.spots} spots remaining.</h3>
+      <h3 className={addClass}>{formatS}</h3>
     </li>
   );
 }
