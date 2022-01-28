deb59081ba884576d8fef7b4572be3bf23d90c71
Yan Sun <tsun812@gmail.com>
2022-01-27 21:16:05 -0800

    add test for blank input and mock onSave function in  Form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
-
+import { fireEvent } from "@testing-library/react";
 import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
@@ -24,5 +24,44 @@ describe("Form", () => {
   it("renders with initial student name", () => {
     const { getByTestId} = render(<Form interviewers={interviewers} student="Lydia Miller-Jones"/>);
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+
+  it("validates that the student name is not blank", () => {
+    /* 1. Create the mock onSave function */
+    const onSave = jest.fn();
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
+    const { getByText} = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+    /* 3. Click the save button */
+    fireEvent.click(getByText("Save"));
+    /* 4. validation is shown */
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+
+    /* 5. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+  
+  it("calls onSave function when the name is defined", () => {
+    /* 1. Create onSave mock function*/
+    const onSave = jest.fn();
+    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+    const { getByText, queryByText} = render(
+      <Form
+        interviewers={interviewers}
+        student="Lydia Miller-Jones"
+        onSave={onSave}
+      />
+    );
+    /* 3. Click the save button */
+    fireEvent.click(getByText("Save"));
+    /* 4. error is not shown*/
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    /* 2. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+
+    /* 3. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 });
