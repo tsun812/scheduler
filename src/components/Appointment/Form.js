e9361b7d6d9ccd4d1e7cd4fdb4c6fe73d3df5db0
Yan Sun <tsun812@gmail.com>
2022-01-27 21:21:42 -0800

    add error state to Form component, display error if student name is null

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button"
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   function reset(){
     setStudent("");
     setInterviewer(null);
@@ -15,6 +16,14 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -29,6 +38,7 @@ export default function Form(props) {
             data-testid = "student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -38,7 +48,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={()=>props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={()=>validate()}>Save</Button>
         </section>
       </section>
     </main>
